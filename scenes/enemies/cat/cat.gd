709c9e094368c5ccb7005237242c08abc21f6e62
comaybay <comaybayy@gmail.com>
2023-03-14 10:40:11 +0700

    basic cat scene and dog scene, test scene

---
+++ scenes/enemies/cat/cat.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 200.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	$AnimationPlayer.play("walk")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	velocity.x = -speed
+
+	move_and_slide()
