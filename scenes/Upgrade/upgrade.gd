dfe3e9f24ad4c1db28b8ee69cedf5518e3cb7498
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-04-26 14:08:58 +0700

    up

--- scenes/Upgrade/upgrade.gd
+++ scenes/Upgrade/upgrade.gd
@@ -1,10 +1,17 @@
 extends Control
 
-
+var upgrade_data 
+var game_data
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var file1 = FileAccess.open("res://resources/game_data/data.json", FileAccess.READ)
+	game_data = JSON.parse_string(file1.get_as_text())
+	var file2 = FileAccess.open("res://resources/game_data/upgrade.json", FileAccess.READ)
+	upgrade_data = JSON.parse_string(file2.get_as_text())
 	$Khung/PhanDau/TieuDe/Xuong/Money.text = "Xương : " + str(Data.money)
-	pass # Replace with function body.
+	
+	file1.close()
+	file2.close()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +21,8 @@ func _process(delta):
 
 func _on_nut_quay_lai_pressed():
 	get_tree().change_scene_to_file("res://scenes/dogBase/dogBase.tscn")
+
+
+func _on_box_pressed(button):
+	$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text = button.text
+	
