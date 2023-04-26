9759e18add5ba6e48fa033f0ea676d1efcce4f89
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-04-26 14:50:57 +0700

    up

--- scenes/Upgrade/upgrade.gd
+++ scenes/Upgrade/upgrade.gd
@@ -1,22 +1,39 @@
 extends Control
 
+const ListItem = preload("res://scenes/Upgrade/box.tscn")
+
 var upgrade_data 
 var game_data
-# Called when the node enters the scene tree for the first time.
+var index = 0 
+
+var Box : Node
+func addItem(value):
+	Box = $Khung/PhanGiua/PhanTren/Items/ScrollContainer/GridContainer
+	
+	var item = ListItem.instantiate()
+	index += 1
+	item.get_node("Button/Number").text = str(index)
+	item.get_node("Button/TextureRect").texture = load(value)
+	
+	$Khung/PhanGiua/PhanTren/Items/ScrollContainer/GridContainer.add_child(item)
+
 func _ready():
+	$Khung/PhanDau/TieuDe/Xuong/Money.text = "Xương : " + str(Data.money)
+	#$Khung/PhanGiua/PhanTren/Items/ScrollContainer/GridContainer/Box/TextureRect.texture = load("res://resources/images/Screenshot (5).png")
 	var file1 = FileAccess.open("res://resources/game_data/data.json", FileAccess.READ)
 	game_data = JSON.parse_string(file1.get_as_text())
+	file1.close()
 	var file2 = FileAccess.open("res://resources/game_data/upgrade.json", FileAccess.READ)
 	upgrade_data = JSON.parse_string(file2.get_as_text())
-	$Khung/PhanDau/TieuDe/Xuong/Money.text = "Xương : " + str(Data.money)
+	file2.close()	
 	
-	file1.close()
-	file2.close()
+	#var group = $Khung/PhanGiua/PhanTren/Items/ScrollContainer/GridContainer.get_children()
+	for i in upgrade_data["detail"].size() :
+		print(upgrade_data["detail"][i]['path'])
+		addItem(upgrade_data["detail"][i]['path'])
+		
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_nut_quay_lai_pressed():
@@ -24,5 +41,6 @@ func _on_nut_quay_lai_pressed():
 
 
 func _on_box_pressed(button):
+	print("add")
 	$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text = button.text
 	
