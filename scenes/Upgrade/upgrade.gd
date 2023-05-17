c3e5eb30128b1cecfd09dfac78a6dd542efcb66e
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-05-17 10:41:35 +0700

    up gia tien upgrade

--- scenes/Upgrade/upgrade.gd
+++ scenes/Upgrade/upgrade.gd
@@ -26,12 +26,13 @@ func sendInfo(ID,detail, nameBox, level, price):
 	detail_index = detail
 	level_index = level
 	name_index = nameBox
+	var gia = str(int(price) * pow(2,int(level) ))
 	price_index = price
 	if (int(level) == 0) :
-		$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text ="Tên : "+ nameBox + "\n" + "Mô tả : " +detail + "\n" + "Giá xương : " + price
+		$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text ="Tên : "+ nameBox + "\n" + "Mô tả : " +detail + "\n" + "Giá xương : " + gia
 		$Khung/PhanGiua/PhanDuoi/TieuDe/NutNangCap.text = "Mua"
 	else :
-		$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text ="Tên : "+ nameBox + "\n" + "Mô tả : " +detail  + "\n" + "Giá xương : " + price+"\n" + "Cấp độ : "  + level 
+		$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text ="Tên : "+ nameBox + "\n" + "Mô tả : " +detail  + "\n" + "Giá xương : " + gia+"\n" + "Cấp độ : "  + level 
 		$Khung/PhanGiua/PhanDuoi/TieuDe/NutNangCap.text = "Nâng cấp"
 	
 func addItem(value):	
@@ -49,11 +50,8 @@ func addItem(value):
 			break
 	
 	# tinh toan so tien dua tren level
-	if (item.get_node("Level").text == "0"):
-		bone = value["price"]
-	else :
-		bone = int(value["price"]) * (1+ int(item.get_node("Level").text))
-	item.get_node("Price").text = str(bone)
+	bone =int(value["price"]) * pow(2,int(item.get_node("Level").text)) 
+	item.get_node("Price").text = str(value["price"])
 	$Khung/PhanGiua/PhanTren/Items/ScrollContainer/GridContainer.add_child(item)
 
 func _on_nut_quay_lai_pressed():
@@ -77,31 +75,31 @@ func reset():
 	file2.close()
 	
 	level_index = str(int(level_index) + 1)
-	price_index = str(int(price_index)/int(level_index) * (int(level_index) +1))
-	$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text = "Tên : "+ name_index + "\n" + "Mô tả : " + str(detail_index) + "\n"+ "Giá xương : " + price_index+"\n" + "Cấp độ : " + level_index
+	var gia = str(int(price_index) * pow(2,int(level_index) ))
+	#price_index = str(int(price_index)/int(level_index) * (int(level_index) +1))
+	$Khung/PhanGiua/PhanDuoi/ThongTin/Label_Item.text = "Tên : "+ name_index + "\n" + "Mô tả : " + str(detail_index) + "\n"+ "Giá xương : " + gia+"\n" + "Cấp độ : " + level_index
 	for i in character_data :			
 		addItem(i)
 
 func _on_nut_nang_cap_pressed():
-	if (game_data["bone"] < int(price_index)) : # ko đủ tiền
-			print("ko đủ tiền")
+	var gia = str(int(price_index) * pow(2,int(level_index) ))
+	#kiem tra tien 
+	if (game_data["bone"] < int(gia)) : # ko đủ tiền
+			print("ko đủ tiền:")
 	else :
 		print("Đủ tiền")
 		if ($Khung/PhanGiua/PhanDuoi/TieuDe/NutNangCap.text == "Nâng cấp"):
 			for obj in game_data["dogs"].size():
-				#kiem tra tien 
-				if (game_data["bone"] < int(price_index)) : # ko đủ tiền
-					print("ko đủ tiền")
-					break
+				
 				#Nang cap / đủ tiền
 				if (str(game_data["dogs"][obj]["ID"]) == str(ID_index)):
-					game_data["bone"] -=  int(price_index)
+					game_data["bone"] -=  int(gia)
 					game_data["dogs"][obj]["level"] += 1				
 					
 		else : #Mua character
 			$Khung/PhanGiua/PhanDuoi/TieuDe/NutNangCap.text = "Nâng cấp"
 			var item ={"ID": ID_index,"level": 1}
-			game_data["bone"] -=  int(price_index)
+			game_data["bone"] -=  int(gia)
 			game_data["dogs"].push_back(item)
 		var file = FileAccess.open("res://resources/save.json", FileAccess.WRITE)
 		var json_data = JSON.stringify(game_data)
