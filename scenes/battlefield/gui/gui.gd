33254c70b2c64ec4454802ca48ff01ca74540156
Thái Chí Bảo <62052850+comaybay@users.noreply.github.com>
2023-05-20 20:45:35 +0700

    Revert "up" (#33)

--- scenes/battlefield/gui/gui.gd
+++ scenes/battlefield/gui/gui.gd
@@ -2,3 +2,10 @@ extends CanvasLayer
 
 func _process(_delta: float) -> void:
 	$MoneyLabel.text = "%s/%s ₵" % [InBattle.money, InBattle.max_money]
+	
+#TODO: remove this later when time scale button is implemented
+func _input(event: InputEvent) -> void: 
+	if event.is_action_pressed("ui_switch_time_scale"):
+		Engine.time_scale += 1
+		if Engine.time_scale > 3:
+			Engine.time_scale = 1
