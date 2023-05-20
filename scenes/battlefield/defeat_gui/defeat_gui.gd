26c5ef0f301a790095d4e75707bfc697266ff0d4
comaybay <comaybayy@gmail.com>
2023-05-20 22:22:52 +0700

    add defeat ui, update victory ui

---
+++ scenes/battlefield/defeat_gui/defeat_gui.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	$AnimationPlayer.play("default")
+	var return_button = $Control/Panel/Button 
+	return_button.pressed.connect(_go_to_dog_base)
+
+func _go_to_dog_base():
+	get_tree().change_scene_to_file("res://scenes/dogBase/dogBase.tscn")
