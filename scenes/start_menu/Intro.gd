4607d170650af3f5873d41b15aadbb28db2074c6
Thái Chí Bảo <62052850+comaybay@users.noreply.github.com>
2023-06-16 09:12:39 +0700

    Intro sequence text (#75)
    * intro sequence text
    
    * fix text render on top

--- scenes/start_menu/Intro.gd
+++ scenes/start_menu/Intro.gd
@@ -3,7 +3,6 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimationPlayer.play("fade in")
 	$SkipButton.pressed.connect(_on_skip)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
