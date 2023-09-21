6e345a92dfcf94de013d79f5be1038f63694f39c
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-09-21 09:27:18 +0700

    UP STOP FMS

--- scenes/characters/dogs/base_dog.gd
+++ scenes/characters/dogs/base_dog.gd
@@ -3,10 +3,14 @@ class_name BaseDog extends Character
 
 # id used to retrive save infomation of a dog character
 @export var name_id: String 
-
+# Kiem tra xem nhan vat co dang bi dieu khien hay khong
+var check_control 
 var check_hover
 
 func _ready() -> void:
+	$Arrow.position =  Vector2(0,0)
+	$Arrow.position.y = -round($CollisionShape2D.shape.extents.y * 2)
+	$Arrow.hide()
 	super._ready()
 	var dog_upgrade = Data.dogs[name_id]
 	
@@ -17,14 +21,45 @@ func _ready() -> void:
 		
 	super._reready()
 	
+	check_control = 0
 	check_hover = 0
 
 
-func _on_mouse_shape_entered():
-	print("mouse in") 
-	pass # Replace with function body.
-
-
 func _on_mouse_entered():
-	print("mouse iádn") 
-	pass # Replace with function body.
+	check_hover = 1
+	if check_control == 0 :
+		$Arrow.show()
+
+func _on_mouse_exited():
+	check_hover = 0
+	if check_control == 0 :
+		$Arrow.hide()
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed :
+		
+		set_control()
+		
+
+func set_control() :
+	if check_control == 0 : # player can control dog
+		var dogs: Array[Node] = get_tree().get_nodes_in_group("dogs")
+		for dog in dogs:
+			dog.check_control = 0
+			$FiniteStateMachine.show()
+			dog.get_node("Arrow").hide()
+			dog.get_node("Arrow").self_modulate =  Color.RED
+			dog.get_node("FiniteStateMachine").auto_control(true)
+		check_control = 1
+		$Arrow.self_modulate =  Color.YELLOW
+		$Arrow.show()
+		$FiniteStateMachine.auto_control(false)
+		
+	else : # set dog to auto fight
+		$Arrow.self_modulate =  Color.RED
+		$Arrow.hide()		
+		check_control = 0
+		$FiniteStateMachine.auto_control(true)
+
+
+
