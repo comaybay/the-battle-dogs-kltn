2fb85d134e982d301ff0b3acafa2aac5c89bc346
comaybay <comaybayy@gmail.com>
2023-12-07 23:14:35 +0700

    basic_battlefield: _battlefield_data -> _stage_data

--- scenes/battlefields/basic_battlefield/basic_battlefield.gd
+++ scenes/battlefields/basic_battlefield/basic_battlefield.gd
@@ -1,4 +1,4 @@
 class_name BasicBattlefield extends Battlefield
 
 ## get theme of the battlefield
-func get_theme() -> String: return _battlefield_data['theme']
+func get_theme() -> String: return _stage_data['theme']
