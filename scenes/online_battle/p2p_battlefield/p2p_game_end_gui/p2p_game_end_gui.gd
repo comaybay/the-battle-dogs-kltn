bbcff4c0a7e3156c5b7acec5861f4823f69cd28e
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2024-01-08 10:31:00 +0700

    up

--- scenes/online_battle/p2p_battlefield/p2p_game_end_gui/p2p_game_end_gui.gd
+++ scenes/online_battle/p2p_battlefield/p2p_game_end_gui/p2p_game_end_gui.gd
@@ -17,10 +17,10 @@ func setup(winner_id: int) -> void:
 	
 	if server_id == SteamUser.STEAM_ID:
 		if winner_id == SteamUser.players[0]['steam_id']:
-			SilentWolf.sw_save_high_scores(SteamUser.players[0]['username'], "high_scores",5)
+			await SilentWolf.sw_save_high_scores(SteamUser.players[0]['username'], "high_scores",5)
 			SilentWolf.sw_save_high_scores(SteamUser.players[1]['username'], "high_scores",-5)
 		else:
-			SilentWolf.sw_save_high_scores(SteamUser.players[0]['username'], "high_scores",-5)
+			await SilentWolf.sw_save_high_scores(SteamUser.players[0]['username'], "high_scores",-5)
 			SilentWolf.sw_save_high_scores(SteamUser.players[1]['username'], "high_scores",5)
 
 	if winner_id == SteamUser.players[0]['steam_id']:
